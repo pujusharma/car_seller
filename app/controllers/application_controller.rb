875b72eb6ab9889f741223bc13541302f3028b68
pujusharma <sharmapuju005@gmail.com>
2023-02-03 10:44:25 +0530

    test changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,10 @@ class ApplicationController < ActionController::Base
     end
 
     def after_sign_in_path_for(resource)
-        root_path
-      end
+      root_path
+    end
   
-      def after_sign_out_path_for(resource)
-        new_user_session_path
-      end
+    def after_sign_out_path_for(resource)
+      new_user_session_path
+    end
 end
